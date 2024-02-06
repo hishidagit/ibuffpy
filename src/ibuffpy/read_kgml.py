23d700c9f269e6c6a1b2853173af0006bd3d849b
hishida <hishida.atsuki.27c@st.kyoto-u.ac.jp>
2024-02-06 01:07:34 +0000

    read kgml

---
+++ src/ibuffpy/read_kgml.py
@@ -0,0 +1,134 @@
+#functions to read kgml files
+import xml.etree.ElementTree as ET
+from . import ReactionNetwork
+import requests
+import time
+
+# download pathway data from KGML
+def get_pathway_kgml(pathway_id, savepath):
+    # download pathway data from KGML API
+    api = 'http://rest.kegg.jp/get/'+pathway_id+'/kgml'
+    # save the downloaded file
+    kgml = requests.get(api).text
+
+    with open(savepath,'w') as f:
+        f.write(kgml)
+    time.sleep(1)
+
+#return a dataframe of reactions
+def get_reactionData(filepath):
+    with open(filepath, 'r') as f:
+        kgml = f.read()
+
+    root = ET.fromstring(kgml)
+
+    reactions = [child for child in root if child.tag == 'reaction']
+
+    reaction_list = []
+    for reac in reactions:
+        reacName=reac.attrib['name']
+        sub_name_list = [sub.attrib['name'] for sub in reac.findall('substrate')]
+
+        pro_name_list = [pro.attrib['name'] for pro in reac.findall('product')]
+        reaction_list.append([reacName,sub_name_list, pro_name_list])
+
+        direction = reac.attrib['type']
+        if direction=='reversible':
+            reaction_list.append([reacName+'_2',pro_name_list,sub_name_list])
+
+    return reaction_list
+
+def make_network_from_kgml(filepath,info=True):
+    reaction_list=get_reactionData(filepath)
+    if len(reaction_list)==0:
+        raise Exception('empty reaction_list')
+    network=ReactionNetwork.ReactionNetwork(reaction_list,info=info)
+    df_reaction=network.to_df()
+
+    if df_reaction.index.duplicated().sum()>0:
+        # same name, not identical reactions
+        # print('reaction name is not identical,', df_reaction.index.duplicated().sum())
+        raise Exception('reaction name is not identical')
+    else:
+        return network
+
+def IDdict(filepath):
+    dict_id=dict()
+    with open(filepath, 'r') as f:
+        kgml = f.read()
+    root = ET.fromstring(kgml)
+
+    entries = [child for child in root if child.tag == 'entry']
+    for entry in entries:
+        dict_id[entry.attrib['id']]=entry.attrib['name']
+    return dict_id
+
+#convert cpdname to id in kgml
+#cpd names need to be identified by IDs in kgml file
+def cpdname2id(cpdname, filepath):
+    sglcpd_id_list=[]
+    dict_id=IDdict(filepath)
+    for elem in dict_id.items():
+        if cpdname in elem[1].split(' '):
+            return elem[0]
+    
+    raise Exception(f'{cpdname} does not exist in {filepath}')
+
+
+def complete_reaction(reaction):
+    # get reaction data from kegg api
+    # reaction=['rn:R01070_2',['cpd:C00118'],['cpd:C05378']]
+
+    reaction_id=reaction[0][:9]
+    lhs,rhs=reaction[1],reaction[2]
+    lhs=[cpd[-6:] for cpd in lhs]
+    rhs=[cpd[-6:] for cpd in rhs]
+
+    api='http://rest.kegg.jp/get/'+reaction_id
+    form_data=requests.get(api).text
+    time.sleep(1)
+    for line in form_data.splitlines():
+        if 'EQUATION' in line:
+            form_line=line
+            break
+    else:
+        raise ValueError('api data is not correct')
+
+    kegg_lhs,kegg_rhs=form_line[12:].split('<=>') #remove 'EQUATION' part
+
+    kegg_lhs_cpds=kegg_lhs.replace(' ','').split('+')
+    kegg_rhs_cpds=kegg_rhs.replace(' ','').split('+')
+    kegg_lhs_cpds_formula = []
+    kegg_rhs_cpds_formula = []
+    for cpd in kegg_lhs_cpds:
+        if cpd[0]=='C':
+            kegg_lhs_cpds_formula.append(cpd)
+        # when the first character is integer, it is a coefficient
+        elif cpd[0].isdigit():
+            coef = int(cpd[:cpd.find('C')])
+            kegg_lhs_cpds_formula += [cpd[cpd.find('C'):]]*coef
+        else:
+            raise ValueError('cpd name is not correct')
+    for cpd in kegg_rhs_cpds:
+        if cpd[0]=='C':
+            kegg_rhs_cpds_formula.append(cpd)
+        # when the first character is integer, it is a coefficient
+        elif cpd[0].isdigit():
+            coef = int(cpd[:cpd.find('C')])
+            kegg_rhs_cpds_formula += [cpd[cpd.find('C'):]]*coef
+        else:
+            raise ValueError('cpd name is not correct')
+    
+    # direction...  True: => False: <+
+    if (not set(lhs).isdisjoint(set(kegg_lhs_cpds_formula))) and (not set(rhs).isdisjoint(set(kegg_rhs_cpds_formula))):
+        direction=True
+    elif (not set(lhs).isdisjoint(set(kegg_rhs_cpds_formula))) and (not set(rhs).isdisjoint(set(kegg_lhs_cpds_formula))):
+        direction=False
+    else:
+        raise ValueError('direction cannot be determined')
+    if direction:
+        reaction_complete=[reaction[0],kegg_lhs_cpds_formula,kegg_rhs_cpds_formula]
+    else:
+        reaction_complete=[reaction[0],kegg_rhs_cpds_formula,kegg_lhs_cpds_formula]
+
+    return reaction_complete
